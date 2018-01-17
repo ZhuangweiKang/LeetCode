3df350d84f0cb4fd7ed88eb4e3edccfe03565322
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-01-17 13:55:43 -0600

    Update LeetCode744

---
+++ LeetCode744/LeetCode744.py
@@ -0,0 +1,17 @@
+'''
+Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+'''
+
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
