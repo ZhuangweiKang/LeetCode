110eb9fa45741764369066fe063fcfc87ca5d24d
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-27 00:44:03 -0500

    update LeetCode235

--- LeetCode235/src/Solution.java
+++ LeetCode235/src/Solution.java
@@ -2,10 +2,13 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode large = p.val > q.val ? p : q;
         TreeNode small = p == large ? q : p;
+        return helper(root, large, small);
+    }
+    private TreeNode helper(TreeNode root, TreeNode large, TreeNode small) {
         if (root == null) return null;
         if ((large.val > root.val && small.val <= root.val)
                 || (large.val >= root.val && small.val < root.val)) return root;
-        if (small.val > root.val) return lowestCommonAncestor(root.right, p, q);
-        return lowestCommonAncestor(root.left, p, q);
+        if (small.val > root.val) return lowestCommonAncestor(root.right, large, small);
+        return lowestCommonAncestor(root.left, large, small);
     }
 }
